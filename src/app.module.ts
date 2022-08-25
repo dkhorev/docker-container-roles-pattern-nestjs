a79e8904e3d94bed73ddac08c1fdfb7d240d0658
dkhorev <me@dkhorev.pro>
2022-08-25 10:41:07 -0400

    proof of concpet for role pattern complete

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { ApiModule } from './api/api.module';
 import { CronModule } from './cron/cron.module';
 import { CommonModule } from './common/common.module';
 import { WorkersModule } from './workers/workers.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ApiModule, CronModule, CommonModule, WorkersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+    }),
+    ApiModule,
+    CronModule,
+    CommonModule,
+    WorkersModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,37 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { Logger } from '@nestjs/common';
+import { ApiModule } from './api/api.module';
+import { CronModule } from './cron/cron.module';
+import { WorkersModule } from './workers/workers.module';
 
 async function bootstrap() {
+  const role = process.env.CONTAINER_ROLE || 'api';
+
+  let loader;
+  switch (role) {
+    case 'api':
+      loader = ApiModule;
+      break;
+    case 'cron':
+      loader = CronModule;
+      break;
+    case 'workers':
+      loader = WorkersModule;
+      break;
+  }
+
   const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
+    loader,
     new FastifyAdapter(),
   );
+
+  const logger = new Logger();
+  logger.log(`Container role: ${role}`);
+
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
