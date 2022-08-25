ddf8eafd51aae7f223519a567cea5e0b36f24592
dkhorev <me@dkhorev.pro>
2022-08-25 11:57:14 -0400

    initial docker compose file, add bull and cron nest modules

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        redis: {
+          host: configService.get('REDIS_HOST') || 'redis',
+          port: +configService.get('REDIS_PORT') || 6379,
+          password: configService.get('REDIS_PASSWORD') || undefined,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
 export class CommonModule {}
